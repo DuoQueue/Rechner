7aa799128417be707b493dc480b4dbdc56be7405
DuoQueue <kim_frank@gmx.de>
2017-03-15 09:15:39 +0100

    3.Version

--- Taschenrechner/Taschenrechner/Form1.Designer.cs
+++ Taschenrechner/Taschenrechner/Form1.Designer.cs
@@ -43,165 +43,180 @@
             this.divButton = new System.Windows.Forms.Button();
             this.multiButton = new System.Windows.Forms.Button();
             this.ergebnisButton = new System.Windows.Forms.Button();
-            this.wurzelButton = new System.Windows.Forms.Button();
+            this.komplettenClearButton = new System.Windows.Forms.Button();
             this.hochzahlx2Button = new System.Windows.Forms.Button();
             this.löschButton = new System.Windows.Forms.Button();
             this.hochzahlnButton = new System.Windows.Forms.Button();
             this.piButton = new System.Windows.Forms.Button();
             this.kommaButton = new System.Windows.Forms.Button();
-            this.displayLabel = new System.Windows.Forms.Label();
+            this.ergebnisTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // zahl9Button
             // 
-            this.zahl9Button.Location = new System.Drawing.Point(114, 122);
+            this.zahl9Button.Location = new System.Drawing.Point(114, 87);
             this.zahl9Button.Name = "zahl9Button";
             this.zahl9Button.Size = new System.Drawing.Size(45, 23);
             this.zahl9Button.TabIndex = 1;
             this.zahl9Button.Text = "9";
             this.zahl9Button.UseVisualStyleBackColor = true;
+            this.zahl9Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl8Button
             // 
-            this.zahl8Button.Location = new System.Drawing.Point(63, 122);
+            this.zahl8Button.Location = new System.Drawing.Point(63, 87);
             this.zahl8Button.Name = "zahl8Button";
             this.zahl8Button.Size = new System.Drawing.Size(45, 23);
             this.zahl8Button.TabIndex = 2;
             this.zahl8Button.Text = "8";
             this.zahl8Button.UseVisualStyleBackColor = true;
+            this.zahl8Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl6Button
             // 
-            this.zahl6Button.Location = new System.Drawing.Point(114, 151);
+            this.zahl6Button.Location = new System.Drawing.Point(114, 116);
             this.zahl6Button.Name = "zahl6Button";
             this.zahl6Button.Size = new System.Drawing.Size(45, 23);
             this.zahl6Button.TabIndex = 3;
             this.zahl6Button.Text = "6";
             this.zahl6Button.UseVisualStyleBackColor = true;
+            this.zahl6Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl3Button
             // 
-            this.zahl3Button.Location = new System.Drawing.Point(114, 180);
+            this.zahl3Button.Location = new System.Drawing.Point(114, 145);
             this.zahl3Button.Name = "zahl3Button";
             this.zahl3Button.Size = new System.Drawing.Size(45, 23);
             this.zahl3Button.TabIndex = 4;
             this.zahl3Button.Text = "3";
             this.zahl3Button.UseVisualStyleBackColor = true;
+            this.zahl3Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl0Button
             // 
-            this.zahl0Button.Location = new System.Drawing.Point(12, 209);
+            this.zahl0Button.Location = new System.Drawing.Point(12, 174);
             this.zahl0Button.Name = "zahl0Button";
             this.zahl0Button.Size = new System.Drawing.Size(96, 23);
             this.zahl0Button.TabIndex = 5;
             this.zahl0Button.Text = "0";
             this.zahl0Button.UseVisualStyleBackColor = true;
+            this.zahl0Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl5Button
             // 
-            this.zahl5Button.Location = new System.Drawing.Point(63, 151);
+            this.zahl5Button.Location = new System.Drawing.Point(63, 116);
             this.zahl5Button.Name = "zahl5Button";
             this.zahl5Button.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.zahl5Button.Size = new System.Drawing.Size(45, 23);
             this.zahl5Button.TabIndex = 6;
             this.zahl5Button.Text = "5";
             this.zahl5Button.UseVisualStyleBackColor = true;
+            this.zahl5Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl2Button
             // 
-            this.zahl2Button.Location = new System.Drawing.Point(63, 180);
+            this.zahl2Button.Location = new System.Drawing.Point(63, 145);
             this.zahl2Button.Name = "zahl2Button";
             this.zahl2Button.Size = new System.Drawing.Size(45, 23);
             this.zahl2Button.TabIndex = 7;
             this.zahl2Button.Text = "2";
             this.zahl2Button.UseVisualStyleBackColor = true;
+            this.zahl2Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl7Button
             // 
-            this.zahl7Button.Location = new System.Drawing.Point(12, 122);
+            this.zahl7Button.Location = new System.Drawing.Point(12, 87);
             this.zahl7Button.Name = "zahl7Button";
             this.zahl7Button.Size = new System.Drawing.Size(45, 23);
             this.zahl7Button.TabIndex = 8;
             this.zahl7Button.Text = "7";
             this.zahl7Button.UseVisualStyleBackColor = true;
+            this.zahl7Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl4Button
             // 
-            this.zahl4Button.Location = new System.Drawing.Point(12, 151);
+            this.zahl4Button.Location = new System.Drawing.Point(12, 116);
             this.zahl4Button.Name = "zahl4Button";
             this.zahl4Button.Size = new System.Drawing.Size(45, 23);
             this.zahl4Button.TabIndex = 9;
             this.zahl4Button.Text = "4";
             this.zahl4Button.UseVisualStyleBackColor = true;
+            this.zahl4Button.Click += new System.EventHandler(this.button_Click);
             // 
             // zahl1Button
             // 
-            this.zahl1Button.Location = new System.Drawing.Point(12, 180);
+            this.zahl1Button.Location = new System.Drawing.Point(12, 145);
             this.zahl1Button.Name = "zahl1Button";
             this.zahl1Button.Size = new System.Drawing.Size(45, 23);
             this.zahl1Button.TabIndex = 10;
             this.zahl1Button.Text = "1";
             this.zahl1Button.UseVisualStyleBackColor = true;
+            this.zahl1Button.Click += new System.EventHandler(this.button_Click);
             // 
             // minusButton
             // 
-            this.minusButton.Location = new System.Drawing.Point(216, 151);
+            this.minusButton.Location = new System.Drawing.Point(216, 116);
             this.minusButton.Name = "minusButton";
             this.minusButton.Size = new System.Drawing.Size(45, 23);
             this.minusButton.TabIndex = 15;
             this.minusButton.Text = "-";
             this.minusButton.UseVisualStyleBackColor = true;
+            this.minusButton.Click += new System.EventHandler(this.grundrechenarten_Click);
             // 
             // plusButton
             // 
-            this.plusButton.Location = new System.Drawing.Point(165, 151);
+            this.plusButton.Location = new System.Drawing.Point(165, 116);
             this.plusButton.Name = "plusButton";
             this.plusButton.Size = new System.Drawing.Size(45, 23);
             this.plusButton.TabIndex = 14;
             this.plusButton.Text = "+";
             this.plusButton.UseVisualStyleBackColor = true;
+            this.plusButton.Click += new System.EventHandler(this.grundrechenarten_Click);
             // 
             // divButton
             // 
-            this.divButton.Location = new System.Drawing.Point(216, 122);
+            this.divButton.Location = new System.Drawing.Point(216, 87);
             this.divButton.Name = "divButton";
             this.divButton.Size = new System.Drawing.Size(45, 23);
             this.divButton.TabIndex = 13;
             this.divButton.Text = "/";
             this.divButton.UseVisualStyleBackColor = true;
+            this.divButton.Click += new System.EventHandler(this.grundrechenarten_Click);
             // 
             // multiButton
             // 
             this.multiButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.multiButton.Location = new System.Drawing.Point(165, 122);
+            this.multiButton.Location = new System.Drawing.Point(165, 87);
             this.multiButton.Name = "multiButton";
             this.multiButton.Size = new System.Drawing.Size(45, 23);
             this.multiButton.TabIndex = 12;
             this.multiButton.Text = "*";
             this.multiButton.UseVisualStyleBackColor = true;
+            this.multiButton.Click += new System.EventHandler(this.grundrechenarten_Click);
             // 
             // ergebnisButton
             // 
             this.ergebnisButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ergebnisButton.Location = new System.Drawing.Point(165, 180);
+            this.ergebnisButton.Location = new System.Drawing.Point(165, 145);
             this.ergebnisButton.Name = "ergebnisButton";
             this.ergebnisButton.Size = new System.Drawing.Size(96, 52);
             this.ergebnisButton.TabIndex = 16;
             this.ergebnisButton.Text = "=";
             this.ergebnisButton.UseVisualStyleBackColor = true;
             // 
-            // wurzelButton
+            // komplettenClearButton
             // 
-            this.wurzelButton.Location = new System.Drawing.Point(165, 74);
-            this.wurzelButton.Name = "wurzelButton";
-            this.wurzelButton.Size = new System.Drawing.Size(45, 23);
-            this.wurzelButton.TabIndex = 22;
-            this.wurzelButton.Text = "√";
-            this.wurzelButton.UseVisualStyleBackColor = true;
+            this.komplettenClearButton.Location = new System.Drawing.Point(165, 49);
+            this.komplettenClearButton.Name = "komplettenClearButton";
+            this.komplettenClearButton.Size = new System.Drawing.Size(45, 23);
+            this.komplettenClearButton.TabIndex = 22;
+            this.komplettenClearButton.Text = "CE";
+            this.komplettenClearButton.UseVisualStyleBackColor = true;
+            this.komplettenClearButton.Click += new System.EventHandler(this.komplettenClearButton_Click);
             // 
             // hochzahlx2Button
             // 
-            this.hochzahlx2Button.Location = new System.Drawing.Point(12, 74);
+            this.hochzahlx2Button.Location = new System.Drawing.Point(12, 49);
             this.hochzahlx2Button.Name = "hochzahlx2Button";
             this.hochzahlx2Button.Size = new System.Drawing.Size(45, 23);
             this.hochzahlx2Button.TabIndex = 21;
@@ -210,7 +225,7 @@
             // 
             // löschButton
             // 
-            this.löschButton.Location = new System.Drawing.Point(216, 74);
+            this.löschButton.Location = new System.Drawing.Point(216, 49);
             this.löschButton.Name = "löschButton";
             this.löschButton.Size = new System.Drawing.Size(45, 23);
             this.löschButton.TabIndex = 20;
@@ -219,7 +234,7 @@
             // 
             // hochzahlnButton
             // 
-            this.hochzahlnButton.Location = new System.Drawing.Point(63, 74);
+            this.hochzahlnButton.Location = new System.Drawing.Point(63, 49);
             this.hochzahlnButton.Name = "hochzahlnButton";
             this.hochzahlnButton.Size = new System.Drawing.Size(45, 23);
             this.hochzahlnButton.TabIndex = 18;
@@ -228,7 +243,7 @@
             // 
             // piButton
             // 
-            this.piButton.Location = new System.Drawing.Point(114, 74);
+            this.piButton.Location = new System.Drawing.Point(114, 49);
             this.piButton.Name = "piButton";
             this.piButton.Size = new System.Drawing.Size(45, 23);
             this.piButton.TabIndex = 17;
@@ -238,29 +253,32 @@
             // kommaButton
             // 
             this.kommaButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.kommaButton.Location = new System.Drawing.Point(114, 209);
+            this.kommaButton.Location = new System.Drawing.Point(114, 174);
             this.kommaButton.Name = "kommaButton";
             this.kommaButton.Size = new System.Drawing.Size(45, 23);
             this.kommaButton.TabIndex = 23;
             this.kommaButton.Text = ",";
             this.kommaButton.UseVisualStyleBackColor = true;
+            this.kommaButton.Click += new System.EventHandler(this.button_Click);
             // 
-            // displayLabel
+            // ergebnisTextBox
             // 
-            this.displayLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.displayLabel.Location = new System.Drawing.Point(12, 9);
-            this.displayLabel.Name = "displayLabel";
-            this.displayLabel.Size = new System.Drawing.Size(249, 62);
-            this.displayLabel.TabIndex = 24;
+            this.ergebnisTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ergebnisTextBox.Location = new System.Drawing.Point(12, 13);
+            this.ergebnisTextBox.Name = "ergebnisTextBox";
+            this.ergebnisTextBox.Size = new System.Drawing.Size(249, 31);
+            this.ergebnisTextBox.TabIndex = 24;
+            this.ergebnisTextBox.Text = "0";
+            this.ergebnisTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // Rechner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 239);
-            this.Controls.Add(this.displayLabel);
+            this.ClientSize = new System.Drawing.Size(273, 206);
+            this.Controls.Add(this.ergebnisTextBox);
             this.Controls.Add(this.kommaButton);
-            this.Controls.Add(this.wurzelButton);
+            this.Controls.Add(this.komplettenClearButton);
             this.Controls.Add(this.hochzahlx2Button);
             this.Controls.Add(this.löschButton);
             this.Controls.Add(this.hochzahlnButton);
@@ -280,9 +298,13 @@
             this.Controls.Add(this.zahl6Button);
             this.Controls.Add(this.zahl8Button);
             this.Controls.Add(this.zahl9Button);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(289, 244);
+            this.MinimumSize = new System.Drawing.Size(289, 244);
             this.Name = "Rechner";
             this.Text = "Rechner";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -303,13 +325,13 @@
         private System.Windows.Forms.Button divButton;
         private System.Windows.Forms.Button multiButton;
         private System.Windows.Forms.Button ergebnisButton;
-        private System.Windows.Forms.Button wurzelButton;
+        private System.Windows.Forms.Button komplettenClearButton;
         private System.Windows.Forms.Button hochzahlx2Button;
         private System.Windows.Forms.Button löschButton;
         private System.Windows.Forms.Button hochzahlnButton;
         private System.Windows.Forms.Button piButton;
         private System.Windows.Forms.Button kommaButton;
-        private System.Windows.Forms.Label displayLabel;
+        private System.Windows.Forms.TextBox ergebnisTextBox;
     }
 }
 
--- Taschenrechner/Taschenrechner/Form1.cs
+++ Taschenrechner/Taschenrechner/Form1.cs
@@ -12,9 +12,32 @@ namespace Taschenrechner
 {
     public partial class Rechner : Form
     {
+        private Double ergebnis = 0;
+        private String grundrechenarten = "";
         public Rechner()
         {
             InitializeComponent();
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+           if(ergebnisTextBox.Text=="0")
+            {
+                ergebnisTextBox.Clear();
+            }
+            Button zahl = (Button)sender;
+            ergebnisTextBox.Text = ergebnisTextBox.Text + zahl.Text;
+        }
+
+        private void komplettenClearButton_Click(object sender, EventArgs e)
+        {
+            ergebnisTextBox.Text="0";
+        }
+
+        private void grundrechenarten_Click(object sender, EventArgs e)
+        {
+            Button grundrechenartenButton = (Button)sender;
+            grundrechenarten = grundrechenartenButton.Text;
         }
     }
 }
